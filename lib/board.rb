1dc0ec459d461093d3d2b92d10216dd8bfa1e409
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-13 13:37:03 +0700

    initial board setup with ability to move pieces

---
+++ lib/board.rb
@@ -0,0 +1,43 @@
+require_relative "piece"
+
+class Board
+  attr_reader :grid
+  def initialize
+    @grid = Array.new(8) {Array.new(8)}
+    setup
+  end
+
+  def setup
+    (0...8).each do |col|
+      @grid[0][col] = Piece.new
+      @grid[1][col] = Piece.new
+      @grid[6][col] = Piece.new
+      @grid[7][col] = Piece.new
+    end
+  end
+
+  def [](row, col)
+    @grid[row][col]
+  end
+
+  def []=(row,col,piece)
+    @grid[row][col] = piece
+  end
+
+  def move_piece(start_pos, end_pos)
+    if self[*start_pos].is_a?(Piece) && self[*end_pos].nil?
+      self[*start_pos], self[*end_pos] = self[*end_pos], self[*start_pos]
+    elsif !self[*start_pos].is_a?(Piece)
+      raise ArgumentError, "There is no chess piece at #{start_pos}"
+    elsif self[*end_pos]
+      raise ArgumentError, "Ending position #{end_pos} is not empty"
+    end
+  end
+end
+
+game = Board.new
+p game[0,0]
+p game[2,3]
+game.move_piece([0,0], [1,3])
+p game[0,0]
+p game[2,3]
